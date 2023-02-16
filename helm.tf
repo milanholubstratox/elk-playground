7b44536e217314478a5a612b0da1ea1816865e02
Milan Holub <milan.holub@tamtamresearch.com>
2023-02-16 01:11:51 +0100

    WIP - fluent-bit+elk

--- helm.tf
+++ helm.tf
@@ -4,18 +4,52 @@ provider "helm" {
   }
 }
 
-# resource "helm_release" "elk" {
-#   name             = "elk-release"
+# resource "helm_release" "eck" {
+#   name             = "elastic"
 #   namespace        = "elastic-system"
 #   create_namespace = "true"
-#   repository       = "https://charts.bitnami.com/bitnami"
-#   chart            = "elasticsearch"
-#   version          = "19.5.10"
-#   set {
-#     name  = "global.kibanaEnabled"
-#     value = "true"
-#   }
+#   repository       = "https://helm.elastic.co"
+#   chart            = "eck-operator"
+#   version          = "2.6.1"
 # }
+
+resource "helm_release" "es" {
+  name             = "es"
+  namespace        = "elastic-system"
+  create_namespace = "true"
+  repository       = "https://helm.elastic.co"
+  chart            = "elasticsearch"
+  version          = "8.5.1"
+  set {
+    name  = "volumeClaimTemplate.resources.requests.storage"
+    value = "10Gi" # default 30Gi
+  }
+  set {
+    name  = "replicas"
+    value = "1" # default 3
+  }
+  depends_on = [k3d_cluster.mycluster]
+}
+
+resource "helm_release" "kibana" {
+  name             = "kibana"
+  namespace        = "elastic-system"
+  create_namespace = "true"
+  repository       = "https://helm.elastic.co"
+  chart            = "kibana"
+  version          = "8.5.1"
+  depends_on = [k3d_cluster.mycluster,helm_release.es]
+}
+
+resource "helm_release" "fluent" {
+  name             = "fluent"
+  namespace        = "logging-system"
+  create_namespace = "true"
+  repository       = "https://fluent.github.io/helm-charts"
+  chart            = "fluent-bit"
+  version          = "0.24.0"
+  depends_on = [k3d_cluster.mycluster,helm_release.es]
+}
 
 resource "helm_release" "monitoring" {
   name             = "monitoring-release"
@@ -24,4 +58,19 @@ resource "helm_release" "monitoring" {
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "kube-prometheus-stack"
   version          = "45.0.0"
+  depends_on = [k3d_cluster.mycluster]
 }
+
+# resource "helm_release" "elk" {
+# name             = "elk-release"
+# namespace        = "elastic-system"
+# create_namespace = "true"
+# repository       = "https://charts.bitnami.com/bitnami"
+# chart            = "elasticsearch"
+# version          = "19.5.10"
+# set {
+# name  = "global.kibanaEnabled"
+# value = "true"
+# }
+# }
+
--- k3d.tf
+++ k3d.tf
@@ -6,5 +6,21 @@ resource "k3d_cluster" "mycluster" {
   name    = "demo"
   servers = 1
   agents  = 2
+  volume {
+    source      = "/etc/machine-id"
+    destination = "/etc/machine-id"
+  }
+  registries {
+    use = ["k3d-docker-io:5001","k3d-elastic-co:5002"]
+    config = <<EOF
+mirrors:
+  "docker.io":
+    endpoint:
+      - http://k3d-docker-io:5001
+  "elastic.co":
+    endpoint:
+      - http://k3d-elastic-co:5002
+EOF
+  }
 }
 
